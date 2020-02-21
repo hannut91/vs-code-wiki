c12b98db2760c45e903ef99179a3859217c61ee0
hannut91 <hannut91@gmail.com>
2020-02-21 20:49:35 +0900

    Fix not working on Windows
    Replace `process.env.HOME` to `path.homedir()` because
    `process.env.HOME` is `undfeind` on Windows.

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,12 @@
 import { workspace } from 'vscode';
+import { homedir } from 'os';
+import { join } from 'path';
 
 export const indexFile = 'index.md';
 
 const configuration = workspace.getConfiguration('wiki');
 export const basePath = configuration.get<string>('basePath')
-  || `${process.env.HOME}/vscode_wiki`;
+  || join(homedir(), 'vscode_wiki');
 
 export const rootFile = `${basePath}/${indexFile}`;
 export const targetRootFile = configuration.get<string>('targetRootFile');
